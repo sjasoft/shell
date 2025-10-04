885fd4672e8b053cff1fb56cc4519805a49ec1db
Samantha Atkins <samantha@sjasoft.com>
2025-10-04 14:33:05 -0400

    wip

--- functions.sh
+++ functions.sh
@@ -1,15 +1,42 @@
-function into_to_git {
+function transcribe_audio {
+    whisperx $1 --verbose False --model medium --language en --output_dir /tmp  --output_format txt
+}    
+
+function transcribe {
+    rm /tmp/video.*
+    yt-dlp -x --cookies-from-browser firefox --audio-format mp3 -o /tmp/video.mp3 $1
+    transcribe_audio /tmp/video.mp3
+    cat /tmp/video.txt | more
+}
+
+function install_alt_java {
+    for file in $1/bin/*
+    do
+	if [ -x $file ]
+	then
+	    filename=`basename $file`
+	    sudo alternatives --install /usr/bin/$filename $filename $file 20000
+	    sudo alternatives --set $filename $file
+      #echo $file $filename
+	fi
+    done
+}
+
+function git_post_init {
     path=$1
-    git_place=${2:-"https://github.com"}
-    git init
+    git_place=${2:-"https://github.com/sjasoft"}
     git commit -m 'first commit'
     git remote add origin $git_place/$path
-    git push -u origin master
+    git push -u origin main
+}
+function into_to_git {
+    git init
+    git_post_init $1
 }
 function install_mongo {
     code=$(lsb_release -c -s)
     sudo apt-get install gnupg
-    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo  apt-key add -
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${code}/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
     sudo apt update && sudo apt install mongodb-org
 }
